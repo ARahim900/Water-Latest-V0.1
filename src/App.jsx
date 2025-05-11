289f9cca8776bd27f2912f2871fc27f710a4cce6
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-05-11 06:12:50 +0400

    Add App.jsx with main component import

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MuscatBayDashboard from './components/MuscatBayDashboard'
+
+function App() {
+  return (
+    <div className="App">
+      <MuscatBayDashboard />
+    </div>
+  )
+}
+
+export default App
